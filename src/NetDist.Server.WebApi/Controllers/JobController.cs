5c041d9e18b53ee89d0cefac5d32bddede1de6e6
Roemer <r.baeriswyl@gmail.com>
2015-01-13 09:55:41 +0100

    - Prepared job controller

--- src/NetDist.Server.WebApi/Controllers/JobController.cs
+++ src/NetDist.Server.WebApi/Controllers/JobController.cs
@@ -1,15 +1,26 @@
-﻿using System.Web.Http;
+﻿using NetDist.Jobs;
+using System;
+using System.Web.Http;
 
 namespace NetDist.Server.WebApi.Controllers
 {
     [RoutePrefix("api/job")]
     public class JobController : ApiController
     {
+        [HttpGet]
         [Route("get")]
-        public IHttpActionResult GetJob()
+        public IHttpActionResult GetJob(Guid clientId)
         {
-            object job = null;
+            var job = WebApiServer.Instance.GetJob(clientId);
             return Ok(job);
+        }
+
+        [HttpPost]
+        [Route("result")]
+        public IHttpActionResult Result(JobResult result)
+        {
+            WebApiServer.Instance.ReceiveResult(result);
+            return Ok();
         }
     }
 }
