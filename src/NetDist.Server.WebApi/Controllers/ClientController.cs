2023b6d0aeb67ba0b269e089745c3837b50623c1
Roemer <r.baeriswyl@gmail.com>
2015-01-14 21:13:34 +0100

    - Merged job and client controller

--- src/NetDist.Server.WebApi/Controllers/ClientController.cs
+++ src/NetDist.Server.WebApi/Controllers/ClientController.cs
@@ -1,8 +1,26 @@
-﻿using System.Web.Http;
+﻿using NetDist.Jobs;
+using System;
+using System.Web.Http;
 
 namespace NetDist.Server.WebApi.Controllers
 {
+    [RoutePrefix("api/client")]
     public class ClientController : ApiController
     {
+        [HttpGet]
+        [Route("getjob")]
+        public IHttpActionResult GetJob(Guid clientId)
+        {
+            var job = WebApiServer.Instance.GetJob(clientId);
+            return Ok(job);
+        }
+
+        [HttpPost]
+        [Route("result")]
+        public IHttpActionResult Result(JobResult result)
+        {
+            WebApiServer.Instance.ReceiveResult(result);
+            return Ok();
+        }
     }
 }
